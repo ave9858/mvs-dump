c569aff73a4ff4ea6bcc220202a5817820abb774
awuctl <awuctl@outlook.com>
2023-02-23 17:57:13 +0100

    helper for getting cookie from credentials

---
+++ get_cookie.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+from selenium.webdriver import Chrome
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support.expected_conditions import url_contains
+
+def get_token(email: str, passwd: str) -> str:
+
+    opts = Options()
+    opts.add_argument('--headless')
+
+    drv = Chrome(options=opts)
+    drv.implicitly_wait(5)
+
+    drv.get('https://my.visualstudio.com/')
+
+    # Fill in email
+    email_field = drv.find_element(By.ID, 'i0116')
+    email_field.send_keys(email + '\n')
+
+    # Fill in password
+    WebDriverWait(drv, 5).until(url_contains('https://login.live.com/'))
+    passwd_field = drv.find_element(By.ID, 'i0118')
+    passwd_field.send_keys(passwd + '\n')
+
+    # "Stay signed in?"..
+    drv.find_element(By.CSS_SELECTOR, 'input[type="submit"]').click()
+
+    # Get cookie..
+    WebDriverWait(drv, 5).until(url_contains('https://my.visualstudio.com/'))
+    token: str = drv.get_cookie('UserAuthentication')['value']
+
+    drv.close()
+    return token
+
+def main():
+
+    credentials = [x.strip() for x in open('creds.txt').read().splitlines()]
+
+    with open('mvs.cookie') as cookie:
+        cookie.write(get_token(*credentials))
+
+if __name__ == '__main__':
+    main()
